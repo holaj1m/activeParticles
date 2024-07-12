50ee029e1ba43e10b9b683e26302290884ff9791
holaj1m <jimcalcetin@gmail.com>
2024-07-12 12:31:30 -0400

    Línea que incorpora la cantidad de particulas

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 int main(){
     int *posX{nullptr}, *posY{nullptr};
+    int particles{};
     double *angle{nullptr};
     std::cout << "Primer commit del repositorio de partículas activas :D" << std::endl;
     return 0;
