da412d145ba036f7740fe19d94ea9fc054328df7
holaj1m <jimcalcetin@gmail.com>
2024-08-05 09:44:42 -0400

    Eliminación del main cpp para dar paso a trabajar con cuda

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(){
-    int *posX{nullptr}, *posY{nullptr};
-    int particles{};
-    double *angle{nullptr};
-    std::cout << "Primer commit del repositorio de partículas activas :D" << std::endl;
-    return 0;
-}
